1d8d3f9c62bd08836230f394bf257b18f3fc8069
1s4ac <isaac.mcustodio@gmail.com>
2022-12-02 01:01:11 -0300

    first structure

--- src/wweb/index.js
+++ src/wweb/index.js
@@ -1,44 +1,42 @@
-const { Client, LocalAuth, MessageMedia,  } = require("whatsapp-web.js");
-const qrcode = require('qrcode-terminal')
-const moment = require('moment')
+const { Client, LocalAuth, MessageMedia } = require("whatsapp-web.js");
+const qrcode = require("qrcode-terminal");
+const moment = require("moment");
 
 const client = new Client({
   authStrategy: new LocalAuth(),
 });
 
-  const start = async () => {
-    try {
-      client.on("qr", (qr) => {
-        qrcode.generate(qr, {small: true})
-      });
+const start = async () => {
+  try {
+    client.on("qr", (qr) => {
+      qrcode.generate(qr, { small: true });
+    });
 
-      client.on('authenticated', () => {
-        console.log("User authenticated successfully🧑‍🚀")
-      })
+    client.on("authenticated", () => {
+      console.log("User authenticated successfully🧑‍🚀");
+    });
 
-      client.on("ready", () => {
-        console.log("Client is ready to zap!🚀");
-      });
+    client.on("ready", () => {
+      console.log("Client is ready to zap!🚀");
+    });
 
-      client.on('message', async (message) => {
+    client.on("message", async (message) => {
+      if (message.isStatus) {
+        return;
+      }
 
-        if (message.isStatus) {
-          return
-        }
+      if (message.type == audio) {
+        console.log("isso é um audio");
+      }
 
-        if (message.type == audio) {
-          if (Date.now() >)
-          message.reply
-        }
+      const chat = await message.getChat();
+      console.log({ from: chat.name, message: message.body });
+    });
 
-        const chat = await message.getChat()
-        console.log({from: chat.name, message: message.body})
-      })
-     
-      client.initialize()
-    } catch (err) {
-      console.log("couldn't connect to whatsapp service.", err);
-    }
+    client.initialize();
+  } catch (err) {
+    console.log("couldn't connect to whatsapp service.", err);
   }
+};
 
-module.exports = {start, saveFile}
+module.exports = { start };
